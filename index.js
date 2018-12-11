ec3d8d40746d54eb7383ef00e41c7d293ccce554
Pablo de Jesús <pag753@gmail.com>
2018-12-11 15:09:02 -0600

    Fin de pruebas express

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
 //Importando librería de express
 const express = require('express')
 //Creando aplicación de express
+const cors = require('cors')
 const app = express()
-//Agregando función para obtener los datos por POST en un json
-//app.use(express.urlencoded())
-app.use(express.urlencoded())
-//Petición GET desde raíz
-app.get('/', (req, res) => {
-    /*res.send('' +
-      '<form method="post">' +
-      '<input type="text" placeholder="Nombre" name="nombre">'+
-      '<input type="text" placeholder="Apellido paterno" name="ap">'+
-      '<input type="text" placeholder="Apellido materno" name="am">'+
-      '<input type="submit" value="Aceptar">'+
-      '</form>'
-    )*/
-    //console.log(req.headers)
-  //console.log(req.header('User-Agent'))
-    //res.send('')
-  //res.redirect('/go-there')
-  //res.redirect('https://google.com')
-  res.redirect(404, '/go-there')
+
+const corsOptions = {
+  origin: 'https://facebook.com'
+}
+
+app.get('/products/:id', cors(corsOptions), (req, res, next) => {
+  res.send('mensajito')
 })
 
-app.get('/mayusculas/:valor', (req, res) => {
-//app.get('/mayuscula(s){1,8}/:valor', (req, res) => {
-    res.send(req.params.valor.toUpperCase())
-    //res.send("Estas aqui")
-})
 
-app.get('/cuadrado/:valor', (req, res) => {
-    //Convertir a numero
-    let numero = req.params.valor * 1
-  //Elevar al cuadrado
-  numero = numero ** 2
-  //Mandarlo
-  //res.send(numero + "")
-  res.send(`${numero}`)
-  //res.send("Estas aqui")
+const server = app.listen(3000, () => {
+  console.log('Listening on port %s', server.address().port)
 })
-
-//Petición POST desde raíz
-app.post('/', (req, res) => {
-    //console.log(req.body)
-    res.send(req.body)
-})
-
-//Función cuando el servidor esté listo
-app.listen(3000, () => console.log('Server ready'))
